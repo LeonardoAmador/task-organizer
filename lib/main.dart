fb393d64ae189029a648d4c46d0c6f399151ffda
Leonardo Amador <amadorleonardo8109@gmail.com>
2024-03-04 08:45:09 -0300

    Improved code structure and implemented hide task functionality.

--- lib/main.dart
+++ lib/main.dart
@@ -9,48 +9,75 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Task Organizer',
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: Colors.blue,
+    return const MaterialApp(
+      home: TaskOrganizer(),
+    );
+  }
+}
+
+class TaskOrganizer extends StatefulWidget {
+  const TaskOrganizer({Key? key}) : super(key: key);
+
+  @override
+  State<TaskOrganizer> createState() => _TaskOrganizerState();
+}
+
+class _TaskOrganizerState extends State<TaskOrganizer> {
+  bool isOpacity = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Task Organizer',
+          style: TextStyle(color: Colors.white),
         ),
-        body: ListView(
+        backgroundColor: Colors.blue,
+      ),
+      body: AnimatedOpacity(
+        opacity: isOpacity ? 1 : 0,
+        duration: const Duration(milliseconds: 800),
+        child: ListView(
           children: [
             Task(
-                taskName: 'Sample Task',
-                imageUrl:
-                    'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large'),
+              taskName: 'Sample Task',
+              imageUrl:
+                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+            ),
             Task(
-                taskName: 'Learning Flutter',
-                imageUrl:
-                    'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large'),
+              taskName: 'Learning Flutter',
+              imageUrl:
+                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+            ),
             Task(
-                taskName: 'Testing',
-                imageUrl:
-                    'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large'),
+              taskName: 'Testing',
+              imageUrl:
+                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+            ),
             Task(
-                taskName: 'Practing',
-                imageUrl:
-                    'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large'),
+              taskName: 'Practicing',
+              imageUrl:
+                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+            ),
             Task(
-                taskName: 'Here we are',
-                imageUrl:
-                    'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large'),
+              taskName: 'Here we are',
+              imageUrl:
+                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+            ),
           ],
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // Add onPressed action for FAB
-          },
-          backgroundColor: Colors.blue,
-          child: const Icon(
-            Icons.add,
-            color: Colors.white,
-          ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            isOpacity = !isOpacity;
+          });
+        },
+        backgroundColor: Colors.blue,
+        child: const Icon(
+          Icons.remove_red_eye,
+          color: Colors.white,
         ),
       ),
     );
