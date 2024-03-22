06668e4ebf49c15b7601ffc1bf826595fa9579e9
Leonardo Amador <amadorleonardo8109@gmail.com>
2024-03-22 11:49:59 -0300

    feat(InitialScreen): Implement dynamic addition of tasks

---
+++ lib/screens/initial_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:task_organizer/data/task_inherited.dart';
+import 'package:task_organizer/screens/form_screen.dart';
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({Key? key}) : super(key: key);
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  bool isOpacity = true;
+
+  void reloadScreen() {
+    setState(() {
+      isOpacity = !isOpacity;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Task Organizer',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+        actions: [
+          IconButton(
+            onPressed: reloadScreen,
+            icon: const Icon(Icons.refresh, color: Colors.white), 
+          ),
+        ],
+      ),
+      body: AnimatedOpacity(
+        opacity: isOpacity ? 1 : 0,
+        duration: const Duration(milliseconds: 800),
+        child: ListView.builder(
+          itemCount: TaskInherited.of(context).taskList.length,
+          itemBuilder: (context, index) {
+            return TaskInherited.of(context).taskList[index];
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context, 
+            MaterialPageRoute(builder: (newContext) => FormScreen(taskContext: context,))
+          );
+        },
+        backgroundColor: Colors.blue,
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
