3c76565d2eaa1dfe841a051b3b45b96193cbbce9
Leonardo Amador <amadorleonardo8109@gmail.com>
2024-03-22 11:47:22 -0300

    style(MainApp): improve the indentation

--- lib/screens/main_app.dart
+++ lib/screens/main_app.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:task_organizer/data/task_inherited.dart';
-import 'package:task_organizer/screens/task_organizer.dart';
+import 'package:task_organizer/screens/initial_screen.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: TaskInherited(child: const TaskOrganizer())
+    return MaterialApp(
+      home: TaskInherited(
+        child: const InitialScreen(),
+      ),
     );
   }
 }
