b0b3a1bb40b9a786ea5094b36f61ff27f2f912c3
Leonardo Amador <amadorleonardo8109@gmail.com>
2024-05-08 21:27:54 -0300

    style(TaskDatabase): change the classs name call when returning the openDatabase method

--- lib/data/task_database.dart
+++ lib/data/task_database.dart
@@ -1,15 +1,15 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
-import 'package:task_organizer/models/task.dart';
+import 'package:task_organizer/models/task_dao.dart';
 
 class TaskDatabase {
   static const String tableName = 'tasks';
 
-  Future<Database> initializeDatabase() async {
+  Future<Database> getDatabase() async {
     final String path = join(await getDatabasesPath(), 'task.db');
 
     return openDatabase(path, onCreate: (db, version) {
-      db.execute(Task.createTableQuery);
+      db.execute(TaskDao.createTableQuery);
     }, version: 1);
   }
 }
